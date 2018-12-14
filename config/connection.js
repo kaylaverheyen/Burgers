7049e4228f71dc1aba063268363ed7bd0eedc5a0
kaylaverheyen <kaylaverheyen@icloud.com>
2018-12-14 10:19:41 -0600

    dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-require("dotenv").config();
+require('dotenv').config();
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,6 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('bacon cheeseburger', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('turkey burger', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('black bean burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('shroom burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('deluxe burger', false);
 
