57bf9f76baab7ba4c524af0881dae4c4313cfb48
kaylaverheyen <kaylaverheyen@icloud.com>
2018-12-22 12:40:36 -0600

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,11 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
 require('dotenv').config();
 console.log("env", process.env.MYSQL_PW);
 
+var mysql = require("mysql");
+
 var connection;
+
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
@@ -14,16 +16,17 @@ if (process.env.JAWSDB_URL) {
         password: process.env.MYSQL_PW,
         database: "burgers_db"
     });
-
-    // Make connection.
-    connection.connect(function (err) {
-        if (err) {
-            console.error("error connecting: " + err.stack);
-            return;
-        }
-        console.log("connected as id " + connection.threadId);
-    });
-
-    // Export connection for our ORM to use.
-    module.exports = connection
 };
+
+
+// Make connection.
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
